77e1f5dbf5178ec2a74d93da84d48c760e7fe7ac
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-07-14 01:38:02 +0530

    Feat: add scrolling effect on the overflow text

--- HTML CSS JS/GSAP Tutorials/Scroll Trigger/script.js
+++ HTML CSS JS/GSAP Tutorials/Scroll Trigger/script.js
@@ -1,41 +1,14 @@
 gsap.registerPlugin(ScrollTrigger);
 
-
-gsap.from('#page1 .box', {
-    scale: 0,
-    duration: 1,
-    rotate: 360,
-});
-
-gsap.from('#page2 .box', {
-    scale: 0,
-    delay: 1,
-    duration: 1,
-    rotate: 360,
+gsap.to('#page2 h1', {
+    transform: 'translateX(-150%)',
     scrollTrigger: {
-        trigger: '#page2 .box',
-        start: 'top center',
-        end: 'bottom center',
-        scrub: true,
-        onEnter: function() {
-            gsap.from('#page2 .box', {
-                scale: 1,
-                duration: 1,
-                rotate: 0,
-            });
-        },
-        onLeave: function() {
-            gsap.from('#page2 .box', {
-                scale: 0,
-                duration: 1,
-                rotate: 360,
-            });
-        }
+        trigger:'#page2',
+        scroller:'body',
+        start:'top 0%',
+        end:'top -100%',
+        markers: true,
+        scrub: 2,
+        pin:true
     }
-});
-
-gsap.from('#page3 .box', {
-    scale: 0,
-    duration: 1,
-    rotate: 360,
 });
