63934a239aafea50a3b6fcb0773efcd7b2512aaa
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-08-14 22:17:09 +0530

    Feat: add the video Carousel of highlights

--- React/iphone-website-clone/src/components/Hero.jsx
+++ React/iphone-website-clone/src/components/Hero.jsx
@@ -39,7 +39,8 @@ const Hero = () =>
       <div className='h-5/6 w-full flex flex-col items-center justify-center'>
         <p id="hero" className='hero-title'>iPhone 15 Pro</p>
         <div className='md:w-10/12 w-9/12'>
-          <video className='pointer-events-none' autoPlay muted playsInline={true}>
+          <video 
+            className='pointer-events-none' autoPlay muted playsInline={true}>
             <source 
               src={videoSrc}
               type='video/mp4'
--- React/iphone-website-clone/src/components/Highlights.jsx
+++ React/iphone-website-clone/src/components/Highlights.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+import { rightImg, watchImg } from "../utils"
+import VideoCarousel from "./VideoCarousel"
 
-const Highlights = () => {
+const Highlights = () =>
+{
+  useGSAP(() =>
+  {
+    gsap.to("#title", {
+      duration: 1,
+      opacity: 1,
+      y: 0,
+      ease: "power1.inOut"
+    }),
+    gsap.to('.link',{
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.1,
+      delay: 0.5,
+      ease: "power1.inOut"
+    })
+  }, [])
   return (
-    <div>Highlights</div>
+    <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
+      <div
+        className="screen-max-width"
+      >
+        <div
+          className="md:flex lg:flex  mb-12 w-full items-end justify-between"
+        >
+          <h1 id="title"
+            className="section-heading"
+          >
+            Get The Highlights
+          </h1>
+          <div className="flex items-end justify-center flex-wrap gap-5">
+            <p className="link">
+              Watch The Film
+              <img className="ml-2" src={watchImg} alt="watch-img" />
+            </p>
+            <p className="link">
+              Watch The Event
+              <img className="ml-2" src={rightImg} alt="watch-img" />
+            </p>
+          </div>
+        </div>
+        <VideoCarousel />
+      </div>
+    </section>
   )
 }
 
