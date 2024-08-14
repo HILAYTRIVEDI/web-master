a1c13853522b391c21b75d8a0a71721001518da2
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-08-15 02:17:30 +0530

    Feat: add the sentry tracking code

--- React/iphone-website-clone/src/main.jsx
+++ React/iphone-website-clone/src/main.jsx
@@ -3,6 +3,20 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://e95757d5e4a819ea8b8bdf1279000417@o4507777636106240.ingest.us.sentry.io/4507777638727680",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0,
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
