9f514a0a09163bb9df89d428b64bf06d67d902d5
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-08-14 16:22:07 +0530

    Feat: add the constants in the nav bar item list

--- React/iphone-website-clone/src/components/Navbar.jsx
+++ React/iphone-website-clone/src/components/Navbar.jsx
@@ -1,24 +1,31 @@
 import {appleImg, bagImg, searchImg} from '../utils'
+import { navLists } from '../constants'
 
 const Navbar = () => {
   return (
-    <header>
-        <nav>
+    <header className='w-full py-5 sm:px-10 px-5 flex justify-between items-center'>
+        <nav className='flex items-center w-full screen-max-width justify-between'>
+            {/* Logo */}
             <img 
                 src={appleImg} 
                 alt="apple logo"
                 width={14}
                 height={18}
             />
-
-            <div>
-                {['Phones', 'MacBooks','Tablet'].map((nav)=>(
-                    <div key={nav}>{nav}</div>
+            {/* Nav Lists */}
+            <div className='flex flex-1 max-sm:hidden items-center justify-center'>
+                  {navLists.map((nav)=>(
+                    <div 
+                        className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all' 
+                        key={nav}>
+                        {nav}
+                    </div>
                 ))}
             </div>
-            <div>
+            {/* Search and Bag */}
+            <div className='flex items-center justify-center'>
                 <img src={searchImg} alt="search icon" width={18} height={18}/>
-                  <img src={bagImg} alt="bag icon" width={18} height={18}/>
+                <img src={bagImg} alt="bag icon" width={18} height={18}/>
             </div>
         </nav>
     </header>
