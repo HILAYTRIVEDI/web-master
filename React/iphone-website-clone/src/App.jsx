6c1cb324b2f79a12e5dbf486f63b51a161f7bbb1
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-08-15 02:29:49 +0530

    update for error

--- React/iphone-website-clone/src/App.jsx
+++ React/iphone-website-clone/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Navbar, Hero, Highlights, Model } from './components'
 
+import * as Sentry from "@sentry/react";
+
 const App = () =>
 {
   return (
     <main className='bg-black'>
+      return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
       <Navbar />
       <Hero />
       <Highlights />
@@ -13,4 +16,4 @@ const App = () =>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
--- React/iphone-website-clone/src/main.jsx
+++ React/iphone-website-clone/src/main.jsx
@@ -1,24 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-  dsn: "https://e95757d5e4a819ea8b8bdf1279000417@o4507777636106240.ingest.us.sentry.io/4507777638727680",
+  dsn: "https://d674932a77e6d9b9ced1190d70fd4691@o4506876178464768.ingest.us.sentry.io/4506876181151744",
   integrations: [
     Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
   ],
   tracesSampleRate: 1.0,
   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  replaysSessionSampleRate: 0.1, 
+  replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
 });
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
