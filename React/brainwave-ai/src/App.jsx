ae69edf819cda53e5a48b52b21d5d8edee6ac103
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-08-15 23:28:21 +0530

    FeaT: add constants

--- React/brainwave-ai/src/App.jsx
+++ React/brainwave-ai/src/App.jsx
@@ -1,14 +1,11 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
-import { Button } from './components'
+import { Button, Header } from './components'
 
 const App = () => {
   return (
     <main className='w-full h-full'>
-      <h1>Brainwave AI</h1>
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
-          <Button className="mt-10" href="#login">
-            Something
-          </Button>
+           <Header />
       </div>
       <ButtonGradient />
     </main>
