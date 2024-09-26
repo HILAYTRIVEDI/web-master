010077a78cc4b2acf3c19334a2739f279b5681ef
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-09-26 20:56:17 +0530

    FeaT: add randomiser and projects in python

---
+++ Python/project1.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+print("Hello " + name + "Welcome to my game")
+
+should_we_play = input("Do you want to play? (y/n): ")
+
+if should_we_play == "y":
+    print("Great! Let's play a game!")
+else:
+    print("Okay, let's play another time.")
