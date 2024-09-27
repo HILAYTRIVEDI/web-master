fb5902a8e9ebaf58de22293538eaf9b5a11769dc
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-09-27 22:54:16 +0530

    Feat: react and flask mini applicaiton

--- React-Flask-Website/client/src/App.jsx
+++ React-Flask-Website/client/src/App.jsx
@@ -1,11 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [members, setMembers] = useState([])
+
+  const fetchMembers = async () => {
+    const response = await fetch('http://localhost:8080/members')
+    const data = await response.json()
+    setMembers(data.members)
+  }
+
+  useEffect(() => {
+    fetchMembers()
+  }, [])
+
 
   return (
-    <></>
+    <>
+        <h1>Members</h1>
+        <ul>
+          {
+            members.map((member, index) => (
+              <li key={index}>{member.name} - {member.age} years old</li>
+            ))
+          }
+        </ul>
+    </>
   )
 }
 
