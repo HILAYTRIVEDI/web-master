93ff7608da220457d957740ecb471f14f4404c1d
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-09-27 22:47:45 +0530

    FeaT : add cros and axios

--- React-Flask-Website/flask-server/server.py
+++ React-Flask-Website/flask-server/server.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, origins='*')
 
 # Members API route
-@app.route('/members')
+@app.route('/members', methods=['GET'])
 def return_members():
     return {
         'members': [
@@ -15,4 +17,4 @@ def return_members():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    app.run(debug=True,port=8080)
