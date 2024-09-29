6fdffb34c15c6ca67129a1dc1c34284718fc72f5
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-09-29 14:33:41 +0530

    Feat: add base setup for app and styles

---
+++ Next/prompter/app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/global.css'
+
+export const metadata = {
+  title: 'Prompter',
+  description: 'Discover the power of AI-powered prompts',
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className='main'>
+                <div className='gradient'></div>
+            </div>
+            <main className='main'>
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
