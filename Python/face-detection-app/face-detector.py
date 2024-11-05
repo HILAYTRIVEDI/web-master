9e70bce8d50f2d626aeb1572434f7862cf488036
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-11-05 16:07:37 +0530

    Feat: add cam support for the face detection

--- Python/face-detection-app/face-detector.py
+++ Python/face-detection-app/face-detector.py
@@ -1,19 +1,42 @@
 import cv2
 
+from random import randrange
+
 def app():
 
     # Load the trained data
     trained_faced_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    # Read the image
-    img = cv2.imread('rdj.jpg')
+    # Open the webcam
+    web_cam = cv2.VideoCapture(0)
 
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    while True:
+        # Read the webcam
+        ret, img = web_cam.read()
 
-    # Show the image
-    cv2.imshow('img', gray)
-    cv2.waitKey()
+        if not ret:
+            break
+
+        # Convert the image to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces
+        faces_coordinates = trained_faced_data.detectMultiScale(gray) # it returns a list of rectangles of faces
+
+        for (x, y, w, h) in faces_coordinates:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Show the image
+        cv2.imshow('img', img)
+        cv2.waitKey(1)
+        
+        # Break the loop if the user presses the 'q' key
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+
+    # Release the webcam
+    web_cam.release()
 
 if __name__ == "__main__":
     app()
