72bbd32629dd789a8ef892cc8946a1615b4ad18e
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2024-11-08 20:55:03 +0530

    Feat: add the classifier for the cars.

--- Python/car-detector/car-detector.py
+++ Python/car-detector/car-detector.py
@@ -1,7 +1,14 @@
+# importing libraries
+import cv2
 
 
 def app():
-    print("Hello World")
+    # Reading the image
+    img = cv2.imread("car-img.jpeg")
+
+
+    # Convert the image to greyscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
 if __name__ == "__main__":
